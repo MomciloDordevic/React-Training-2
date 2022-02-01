a756cfecccd1ab2b279a2abc0d39515326cf0cde
Momcilo <qwertyumom@gmail.com>
2022-02-01 11:13:09 +0100

    Test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUserslist] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserslist((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
